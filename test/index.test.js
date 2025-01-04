c416ca7d291a1cc8fd7998557f61b6b847c7e2cc
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-01-05 00:14:47 +0530

    auntication tests done

--- test/index.test.js
+++ test/index.test.js
@@ -23,5 +23,50 @@ describe("Authentication", () => {
     expect(sendAgainRes.status).toBe(400);
   });
 
-  
+  test("Username with empty string should not be allowed", async () => {
+    const username = "";
+    const password = "password";
+    const type = "user";
+
+    const res = await axios.post(`${backendUrl}/api/v1/signup`, {
+      username,
+      password,
+      type,
+    });
+    expect(res.status).toBe(400);
+  });
+
+  test("Signin is good if user exists", async () => {
+    const username = "user" + Math.random();
+    const password = "12344232";
+    const type = "user";
+
+    await axios.post(`${backendUrl}/api/v1/signup`, {
+      username,
+      password,
+      type,
+    });
+
+    const res = await axios.post(`${backendUrl}/api/v1/signin`, {
+      username,
+      password,
+    });
+
+    expect(res.status).toBe(200);
+    expect(res.data).toHaveProperty("token");
+  });
+
+  test("Signin is fails if username and password is incorrect", async () => {
+    const username = "user" + Math.random();
+    const password = "12344232";
+
+    const res = await axios.post(`${backendUrl}/api/v1/signin`, {
+      username: "wrong" + username,
+      password,
+    });
+
+    expect(res.status).toBe(403);
+    expect(res.data).toHaveProperty("message");
+  });
+
 });
