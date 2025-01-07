acc1d1cbd2dd19c7bcf98c00fa57a52fec3672a2
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-01-08 02:46:04 +0530

    fix zod types

--- project-metaverse/apps/http/src/types/index.ts
+++ project-metaverse/apps/http/src/types/index.ts
@@ -1,12 +1,12 @@
 import z from "zod"
 
 export const SignupSchema = z.object({
-    username: z.string(),
-    password: z.string(),
+    username: z.string().min(3),
+    password: z.string().min(4),
     type: z.enum(["user", "admin"])
 });
 
 export const SigninSchema = z.object({
-    username: z.string(),
-    password: z.string()
+    username: z.string().min(3),
+    password: z.string().min(4)
 });
