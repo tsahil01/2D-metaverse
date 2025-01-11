576470e2e2620eb58e6a9b00425e40bac15a8e77
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-01-11 16:47:34 +0530

    added cors

--- project-metaverse/apps/http/src/index.ts
+++ project-metaverse/apps/http/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import router from "./routes";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3000;
 export const JWT_SECRET = process.env.JWT_SECRET || "secret";
 const app = express();
 app.use(express.json())
+app.use(cors())
 app.use('/api/v1', router);
 
 app.listen(PORT, () => console.log(`BE running on port ${PORT}`))
