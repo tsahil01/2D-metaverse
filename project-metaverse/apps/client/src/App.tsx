a71cff118208aa0f7e8f7cfb0ae690c26b9c5e0c
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-01-14 15:45:07 +0530

    client added

--- project-metaverse/apps/client/src/App.tsx
+++ project-metaverse/apps/client/src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { AuthPage } from "./components/ui/auth/page"
 import { Dashboard } from "./components/dashboard/dashboard"
+
 function App() {
   const token = localStorage.getItem("token")
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={
-          token ? <Dashboard/> : <AuthPage />
+          token ? <Dashboard /> : <AuthPage />
         } />
         <Route path="/signup" element={<AuthPage signup={true} />} />
         <Route path="/login" element={<AuthPage signup={false} />} />
