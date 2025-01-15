5dd018f301d03eb05c39f4c32eb253270c890190
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-01-16 01:33:16 +0530

    add types to client

--- project-metaverse/apps/client/src/lib/types.ts
+++ project-metaverse/apps/client/src/lib/types.ts
@@ -6,3 +6,34 @@ export interface SpaceCardProps {
     thumbnail: string | null;
     creatorId: string;
 }
+
+
+export interface ElementInterface {
+    id: string;
+    name: string;
+    imageUrl: string;
+    width: number;
+    height: number;
+    static: boolean;
+}
+
+export interface ElementWithPositionInterface extends ElementInterface {
+    x: number;
+    y: number;
+}
+
+export interface MapElementInterface {
+    id: string;
+    x: number;
+    y: number;
+    element: ElementInterface;
+}
+
+export interface MapInterface {
+    id: string;
+    name: string;
+    thumbnail: string;
+    height: number;
+    width: number;
+    mapElements: MapElementInterface[];
+}
