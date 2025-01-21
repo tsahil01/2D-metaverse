bd3f4a7eca9ac276af5135eba126d7e90b450f15
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-01-21 20:25:03 +0530

    refactor SpaceMain component and remove GettingSpace; add SpaceInit for improved space loading UI

--- project-metaverse/apps/client/src/components/space/main.tsx
+++ project-metaverse/apps/client/src/components/space/main.tsx
@@ -1,7 +1,118 @@
-import { GettingSpace } from "./gettingSpace";
+import { BACKEND_URL } from "@/lib/config";
+import { useEffect, useState } from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { avatarAtom } from "@/lib/atom/avatarAtom";
+import { spaceElementsAtom } from "@/lib/atom/spaceElementsAtom";
+import { dimensionsAtom } from "@/lib/atom/dimensionsAtom";
+import { tokenAtom } from "@/lib/atom/tokenAtom";
+import { userNameAtom } from "@/lib/atom/userNameAtom";
+import { MapElementInterface } from "@/lib/types";
+import { useNavigate } from "react-router-dom";
+import { SpaceInit } from "./spaceInit";
 
-export function SpaceMain() {
+export function SpaceMain({ spaceId = "cm628eno4000hk41j471e3kuo" }: { spaceId: string }) {
+    const [isLoading, setIsLoading] = useState(true);
+    const [spaceName, setSpaceName] = useState<string | null>(null);
+
+    const [token, setToken] = useRecoilState(tokenAtom);
+    const [avatar, setAvatar] = useRecoilState(avatarAtom);
+    const setDimensions = useSetRecoilState(dimensionsAtom);
+    const setElements = useSetRecoilState(spaceElementsAtom);
+    const [name, setName] = useRecoilState(userNameAtom);
+    const [showSpace, setShowSpace] = useState(false);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const storedToken = localStorage.getItem("token");
+        if (!storedToken) {
+            alert("You need to be logged in to access this page");
+            navigate("/login");
+        } else {
+            setToken(storedToken);
+        }
+    }, []);
+
+    useEffect(() => {
+        setIsLoading(true);
+        if (!token) return;
+        async function getSpace() {
+            try {
+                const res = await fetch(`${BACKEND_URL}/space/${spaceId}`, {
+                    method: "GET",
+                    headers: {
+                        contentType: "application/json",
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+                const data = await res.json();
+                console.log(data);
+                if (data === null) {
+                    alert("Space not found");
+                    navigate("/");
+                    return;
+                }
+                setSpaceName(data.name);
+                setDimensions(data.dimensions || null);
+                setElements(
+                    data.elements.map((e: MapElementInterface) => ({
+                        x: e.x,
+                        y: e.y,
+                        id: e.element.id,
+                        name: e.element.name,
+                        imageUrl: e.element.imageUrl,
+                        width: e.element.width,
+                        height: e.element.height,
+                        static: e.element.static,
+                    }))
+                );
+            } catch (error) {
+                console.error("Error fetching space data:", error);
+            }
+        };
+
+        async function getAvatar() {
+            const res = await fetch(`${BACKEND_URL}/user/metadata`, {
+                method: "GET",
+                headers: {
+                    contentType: "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            const data = await res.json();
+            if (data === null) {
+                alert("Avatar not found");
+                navigate("/");
+                return;
+            }
+            setAvatar(data);
+        };
+        getAvatar();
+        getSpace();
+        setIsLoading(false);
+    }, [token, spaceId]);
+
+    if (isLoading) {
+        return <div className="w-screen flex justify-center items-center min-h-screen bg-gradient-to-b from-indigo-100 via-white to-white">
+            <div className="text-white">Loading...</div>
+        </div>
+    }
+
+    function handleClick() {
+        if (!name) {
+            alert("Please enter your name");
+            return;
+        } else {
+            setShowSpace(true);
+        }
+    }
+
     return (
-        <GettingSpace spaceId="cm628eno4000hk41j471e3kuo" />
+        <>
+            <div className="min-h-screen bg-gradient-to-b from-indigo-300 via-white to-white w-screen">
+                <SpaceInit spaceName={spaceName || ""} avatarUrl={avatar?.avatarUrl || ""} handleClick={handleClick} />
+
+            </div>
+        </>
     )
 }
---
+++ project-metaverse/apps/client/src/components/space/spaceInit.tsx
@@ -0,0 +1,38 @@
+import { Input } from "../ui/input";
+
+interface SpaceInitProps {
+    spaceName: string;
+    avatarUrl: string;
+    handleClick: () => void;
+}
+
+export function SpaceInit({ spaceName, avatarUrl, handleClick }: SpaceInitProps) {
+    return (
+        <div className="flex flex-col gap-4 space-y-6 h-screen justify-center items-center mx-auto">
+            <div className="text-3xl font-bold text-center">
+                Welcome to {" "}
+                <div className="text-3xl font-bold text-indigo-600 text-center">
+                    {spaceName}
+                </div>
+            </div>
+            <div className="flex flex-col max-w-lg mx-auto justify-center w-full gap-">
+                <div
+                    className="w-[32px] h-[64px] my-auto bg-no-repeat bg-[length:512px_64px] scale-150 mx-auto mb-9"
+                    style={{
+                        backgroundImage: `url(${avatarUrl})`,
+                        backgroundPosition: "0px 0px",
+                    }}
+                ></div>
+                <div className="grid w-full items-center gap-4 my-auto">
+                    <div className="flex flex-col space-y-1.5">
+                        <Input id="name" placeholder="Enter Your Name" className="p-6 bg-slate-300 font-bold text-center" onChange={(e) => setName(e.target.value)} />
+                    </div>
+                    <button className="inline-flex font-bold items-center justify-center px-8 py-4 rounded-full bg-gradient-to-r from-indigo-600 to-violet-600 text-white shadow-lg shadow-indigo-500/25 hover:shadow-indigo-500/35 transform hover:-translate-y-0.5 transition-all duration-200"
+                        onClick={handleClick}>
+                        {"Join Space"}
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
